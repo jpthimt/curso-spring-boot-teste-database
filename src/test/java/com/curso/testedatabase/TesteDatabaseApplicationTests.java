5d8df138d207ef3f07e030b81d7b7627e53ae71b
jpthimt <joaopedro10thim@gmail.com>
2023-07-18 15:16:49 -0300

    atualização

--- src/test/java/com/curso/testedatabase/TesteDatabaseApplicationTests.java
+++ src/test/java/com/curso/testedatabase/TesteDatabaseApplicationTests.java
@@ -1,13 +1,13 @@
-package com.curso;
+package com.curso.testedatabase;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CursoUdemyApplicationTests {
+class TesteDatabaseApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
